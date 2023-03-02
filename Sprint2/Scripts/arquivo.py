9aad77413df51a5fa5eb64c705400a3e2a3bbec1
Dani Melo <66283945+DaniM-R@users.noreply.github.com>
2023-03-01 22:01:44 -0300

    Script final
    Script final com todas as questões e usando paginação

--- Sprint2/Scripts/arquivo.py
+++ Sprint2/Scripts/arquivo.py
@@ -7,7 +7,7 @@ url = 'https://api.github.com/graphql'
 
 query = '''
 query {
-  search(query: "stars:>100", type: REPOSITORY, first: 20) {
+  search(query: "stars:>100", type: REPOSITORY, first: 20, after: null) {
     nodes {
       ... on Repository {
         nameWithOwner
@@ -31,32 +31,44 @@ query {
         updatedAt
       }
     }
+    pageInfo {
+        endCursor
+        hasNextPage
+    }
   }
 }
 '''
 
-response = requests.post(url, headers=headers, json={"query": query})
+count = 0
+endCursor = None
 
-data = response.json()['data']['search']['nodes']
-
-with open('repos.csv', mode='w', encoding='utf-8', newline='') as csv_file:
+with open('script.csv', mode='w', encoding='utf-8', newline='') as csv_file:
     fieldnames = ['nameWithOwner', 'stargazerCount', 'age', 'pullRequests', 'releases', 'primaryLanguage',
                   'issues', 'closedIssues', 'issuesRatio', 'updatedAt']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
 
-    for repo in data:
-        language = repo['primaryLanguage']['name'] if repo['primaryLanguage'] is not None else ""
-        created_at = datetime.strptime(repo['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
-        age = (datetime.now() - created_at).days
-        issues = repo['issues']['totalCount'] if 'issues' in repo else 0
-        closed_issues = repo['closedIssues']['totalCount'] if 'closedIssues' in repo else 0
-        issues_ratio = closed_issues / issues if issues != 0 else 'N/A'
-        writer.writerow({'nameWithOwner': repo['nameWithOwner'], 'stargazerCount': repo['stargazerCount'],
-                         'age': age, 'pullRequests': repo['pullRequests'],
-                         'releases': repo['releases'], 'primaryLanguage': repo['primaryLanguage'],
-                         'issues': repo['issues'], 'closedIssues': repo['closedIssues'],
-                         'issuesRatio': issues_ratio, 'updatedAt': repo['updatedAt']})
+    while count < 1000:
+        if count > 19:
+            query.replace('after: null', 'after: "%s"' % endCursor)
 
+        response = requests.post(url, headers=headers, json={"query": query})
+        endCursor = response.json()['data']['search']['pageInfo']['endCursor']
+
+        data = response.json()['data']['search']['nodes']
+
+        for repo in data:
+            language = repo['primaryLanguage']['name'] if repo['primaryLanguage'] is not None else ""
+            created_at = datetime.strptime(repo['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
+            age = (datetime.now() - created_at).days
+            issues = repo['issues']['totalCount'] if 'issues' in repo else 0
+            closed_issues = repo['closedIssues']['totalCount'] if 'closedIssues' in repo else 0
+            issues_ratio = closed_issues / issues if issues != 0 else 'N/A'
+            writer.writerow({'nameWithOwner': repo['nameWithOwner'], 'stargazerCount': repo['stargazerCount'],
+                             'age': age, 'pullRequests': repo['pullRequests'],
+                             'releases': repo['releases'], 'primaryLanguage': repo['primaryLanguage'],
+                             'issues': repo['issues'], 'closedIssues': repo['closedIssues'],
+                             'issuesRatio': issues_ratio, 'updatedAt': repo['updatedAt']})
+            count += 1
 
